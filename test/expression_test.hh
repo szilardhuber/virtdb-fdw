474a6653d566e848bf7a78ac85d22141e16a2283
szilardhuber <szilard.huber@gmail.com>
2014-07-28 11:37:55 +0200

    Added a very basic/trivial test

--- test/expression_test.hh
+++ test/expression_test.hh
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "../src/expression.hh"
 #include <memory>
+#include <string>
 
 // the purpose of these tests is to make sure
 // gtest includes and libs are properly integrated
@@ -19,6 +20,23 @@ namespace virtdb
             int a = 0;
             std::shared_ptr<Expression> expression(new Expression);
             EXPECT_NE(expression.get(), nullptr);
+        });
+    }
+
+    TEST_F(ExpressionTest, BasicFunctionality) {
+        EXPECT_NO_THROW(
+        {
+            int a = 0;
+            std::shared_ptr<Expression> expression(new Expression);
+            std::string name = "VariableName";
+            std::string operand = "==!=";
+            std::string value = "42";
+            expression->set_variable(name);
+            expression->set_operand(operand);
+            expression->set_value(value);
+            EXPECT_EQ(expression->get_message()->mutable_simple()->variable(), name);
+            EXPECT_EQ(expression->get_message()->operand(), operand);
+            EXPECT_EQ(expression->get_message()->mutable_simple()->value(), value);
         });
     }
 }
