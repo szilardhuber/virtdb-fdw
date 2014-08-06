ffa065311b2a8f8e7e2d8030150386be7bdef8c3
szilardhuber <szilard.huber@gmail.com>
2014-08-06 13:35:08 +0200

    Compiling new files as well.

--- common.mk
+++ common.mk
@@ -1,7 +1,9 @@
 # BUILD_ROOT is required
 # XXX EXTRA_CLEAN = $(EXTENSION)--$(EXTVERSION).sql
+COMMON_FILE_NAMES = expression.cc query.cc receiver_thread.cc
 PROTO_FILE_NAMES = common.proto meta_data.proto db_config.proto data.proto
-PROTO_FILES = $(patsubst %.proto,$(BUILD_ROOT)/src/proto/%.proto,$(PROTO_FILE_NAMES)) 
+PROTO_FILES = $(patsubst %.proto,$(BUILD_ROOT)/src/proto/%.proto,$(PROTO_FILE_NAMES))
+COMMON_SOURCES =  $(patsubst %.cc,$(BUILD_ROOT)/src/%.cc,$(COMMON_FILE_NAMES))
 PROTO_SOURCES = $(patsubst %.proto,%.pb.cc,$(PROTO_FILES))
 PROTO_OBJECTS = $(patsubst %.pb.cc,%.pb.o,$(PROTO_SOURCES))
 ZMQ_LDFLAGS := $(shell pkg-config --libs libzmq)
@@ -18,15 +20,19 @@ GTEST_LIBS :=  $(GTEST_LIBDIR)/libgtest.a
 GTEST_LDFLAGS := $(GTEST_LIBS) -L$(GTEST_LIBDIR)
 GTEST_CFLAGS := -I$(GTEST_INCLUDE)
 
+COMMON_OBJS = $(patsubst %.cc,%.o,$(COMMON_SOURCES)) $(PROTO_OBJECTS)
+
 # FIXME on Windows
 FIX_CXX_11_BUG =
-LINUX_LDFLAGS =  
+LINUX_LDFLAGS =
 ifeq ($(shell uname), Linux)
 FIX_CXX_11_BUG =  -Wl,--no-as-needed
 LINUX_LDFLAGS =  -pthread
 endif
 
-CXXFLAGS += -std=c++11 -fPIC $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_CFLAGS) $(ZMQ_CFLAGS)
+
+
+CXXFLAGS += -std=c++11 -fPIC $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_CFLAGS) $(ZMQ_CFLAGS) $(CPPFLAGS)
 LDFLAGS += $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_LDFLAGS) $(ZMQ_LDFLAGS)
 
 %.pb.o: %.pb.cc
@@ -39,4 +45,3 @@ $(PROTOBUF_PATH)/data.pb.cc: $(PROTOBUF_PATH)/common.pb.cc
 $(PROTOBUF_PATH)/meta_data.pb.cc: $(PROTOBUF_PATH)/common.pb.cc
 
 $(PROTOBUF_PATH)/db_config.pb.cc: $(PROTOBUF_PATH)/meta_data.pb.cc
-
