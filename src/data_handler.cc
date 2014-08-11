47e32731dc8267869c40073af02f3b2b2388285b
szilardhuber <szilard.huber@gmail.com>
2014-08-11 12:37:44 +0200

    FDW is no able to receive data in chunks.

--- src/data_handler.cc
+++ src/data_handler.cc
@@ -25,17 +25,32 @@ void data_handler::push(std::string name, virtdb::interface::pb::Column new_data
 
 void data_handler::push(int column_number, virtdb::interface::pb::Column new_data)
 {
-    if (data.count(column_number) == 1)
+    if (data.count(column_number) != 1)
     {
-        std::string error_string = "Error! Column already added for this query: " + std::to_string(column_number);
-        throw std::invalid_argument(error_string);
+        data[column_number] = std::vector<virtdb::interface::pb::Column>();
     }
-    data[column_number] = new_data;
+    data[column_number].push_back(new_data);
 }
 
 bool data_handler::received_data() const
 {
-    return data.size() == columns_count;
+    if (data.size() != columns_count)
+    {
+        return false;
+    }
+    int ended = 0;
+    for (auto it = data.begin(); it != data.end(); it++)
+    {
+        for (auto inner_it = it->second.begin(); inner_it != it->second.end(); inner_it++)
+        {
+            if (inner_it->endofdata())
+            {
+                ended += 1;
+                break;
+            }
+        }
+    }
+    return ended == columns_count;
 }
 
 
@@ -50,38 +65,62 @@ bool data_handler::read_next()
     {
         return false;
     }
+    if (inner_cursor >= data.begin()->second[current_chunk].data().isnull_size() - 1)
+    {
+        inner_cursor = -1;
+        current_chunk += 1;
+    }
+    inner_cursor++;
     cursor++;
     return true;
 }
 
 bool data_handler::is_null(int column_number) const
 {
-    if (data.count(column_number) != 1)
-    {
-        return true;
+    try {
+        if (data.count(column_number) != 1)
+        {
+            return true;
+        }
+        if (cursor < 0 || cursor >= data_length())
+        {
+            std::string error_string = "Error! Invalid row: " + std::to_string(cursor);
+            throw std::invalid_argument(error_string);
+        }
+        return data.find(column_number)->second[current_chunk].data().isnull(inner_cursor);
     }
-    if (cursor < 0 || cursor >= data_length())
+    catch(const ::google::protobuf::FatalException & e)
     {
-        std::string error_string = "Error! Invalid row: " + std::to_string(cursor);
+        std::string error_string = "Error [" + std::to_string(__LINE__) + "]! Inner_cursor: " + std::to_string(inner_cursor) + " " + e.what();
         throw std::invalid_argument(error_string);
     }
-    return data.find(column_number)->second.data().isnull(cursor);
 }
 
 const std::string* const data_handler::get_string(int column_number) const
 {
-    if (is_null(column_number))
-        return NULL;
-    return &data.find(column_number)->second.data().stringvalue(cursor);
+    try {
+        if (is_null(column_number))
+            return NULL;
+        return &data.find(column_number)->second[current_chunk].data().stringvalue(inner_cursor);
+    }
+    catch(const ::google::protobuf::FatalException & e)
+    {
+        std::string error_string = "Error [" + std::to_string(__LINE__) + "]! Inner_cursor: " + std::to_string(inner_cursor) + " " + e.what();
+        throw std::invalid_argument(error_string);
+    }
 }
 
 int data_handler::data_length() const
 {
+    int count = 0;
     if (received_data())
     {
-        return data.begin()->second.data().isnull_size();
+        for (auto it = data.begin()->second.begin(); it != data.begin()->second.end(); it++)
+        {
+            count += it->data().isnull_size();
+        }
     }
-    return 0;
+    return count;
 }
 
 } // namespace virtdb
--- test/data_handler_test.hh
+++ test/data_handler_test.hh
@@ -2,6 +2,7 @@
 #include "../src/data_handler.hh"
 #include "../src/util.hh"
 #include <string>
+#include <iostream>
 
 // the purpose of these tests is to make sure
 // gtest includes and libs are properly integrated
@@ -42,6 +43,7 @@ namespace virtdb
 
         data_handler handler(the_query);
         virtdb::interface::pb::Column data = generate_string_data(n_rows, true);
+        data.set_endofdata(true);
         handler.push(0, data);
 
         EXPECT_NO_THROW(
@@ -75,7 +77,9 @@ namespace virtdb
             std::vector<virtdb::interface::pb::Column> columns;
             for (int i = 0; i < n_columns; i++)
             {
-                columns.push_back(generate_string_data(n_rows, n_columns, true));
+                virtdb::interface::pb::Column data = generate_string_data(n_rows, n_columns, true);
+                data.set_endofdata(true);
+                columns.push_back(data);
                 handler.push(i, columns[i]);
             }
 
@@ -94,6 +98,51 @@ namespace virtdb
                         ASSERT_EQ(*handler.get_string(column_index), columns[column_index].data().stringvalue(row_index));
                     }
                 }
+            }
+        });
+    }
+
+    TEST_F(data_handler_test, push_chunks)
+    {
+        EXPECT_NO_THROW(
+        {
+            int chunk_size = 7;
+            int n_rows = 1000;
+            query the_query;
+            the_query.set_table_name( "Query test" );
+            the_query.add_column( gen_random(colname_length) );
+            data_handler handler(the_query);
+
+            std::vector<virtdb::interface::pb::Column> chunks;
+            int pushed_rows = 0;
+            int chunk_index = 0;
+            while (pushed_rows < n_rows)
+            {
+                int current_size = n_rows - pushed_rows > chunk_size ? chunk_size : n_rows - pushed_rows;
+                virtdb::interface::pb::Column data = generate_string_data(current_size, 1, true);
+                data.set_endofdata(false);
+                pushed_rows += current_size;
+                if (pushed_rows == n_rows)
+                {
+                    data.set_endofdata(true);
+                }
+                chunks.push_back(data);
+                handler.push(0, chunks[chunk_index]);
+                if (pushed_rows < n_rows)
+                {
+                    ASSERT_NE(true, handler.received_data());
+                }
+                else
+                {
+                    ASSERT_EQ(true, handler.received_data());
+                }
+                chunk_index++;
+            }
+
+            for (int row_index = 0; row_index < n_rows; row_index++)
+            {
+                ASSERT_EQ(true, handler.has_data());
+                ASSERT_EQ(true, handler.read_next());
             }
         });
     }
